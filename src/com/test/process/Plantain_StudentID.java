f0ed0d4c8c658671c6de893b566afb2598c65196
Vũ Văn Minh <60693215+quanglecs@users.noreply.github.com>
2022-02-18 12:46:16 +0700

    Update Plantain_StudentID.java

--- src/com/test/process/Plantain_StudentID.java
+++ src/com/test/process/Plantain_StudentID.java
@@ -1,8 +1,6 @@
 package com.test.process;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -10,9 +8,6 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class Plantain_StudentID {
@@ -46,7 +41,6 @@ public class Plantain_StudentID {
 			this.wPlan_StuID = wPlan;
 		}
 	}
-
 	// crop lấy ra ô đen mã đề or MSSV
 	public Mat imgPlan_StuID(Mat boxImgPlan_stuID) {
 		Set<Rect> listRect = getTwoNumber(boxImgPlan_stuID);
@@ -59,7 +53,6 @@ public class Plantain_StudentID {
 		int yP_S = rectOn.y + rectOn.height;
 		int wP_S = Math.abs(wBoxPlan_StuID - xP_S);
 		int hP_S = Math.abs(rectBottom.y - yP_S);
-//		System.out.println(xP_S + " " + yP_S + " " + wP_S + " " + hP_S);
 		return cropImgResult(xP_S, yP_S, wP_S, hP_S);
 
 	}
@@ -71,29 +64,25 @@ public class Plantain_StudentID {
 		Set<Rect> rectTree = new TreeSet<Rect>(RectCompareNoise.RECT_COMPARE);
 		for (int i = 0; i < contours.size(); i++) {
 			Rect rect = Imgproc.boundingRect(contours.get(i));
-//			System.err.println(rect);
-			if (rect.area() > 280 && rect.area() < 600) {
-				if (((rect.y > 5 && rect.y < 60) || (rect.y > 290 && rect.y < hBoxP_StuID)) && (rect.x < 38)
-						&& (rect.width > 15 && rect.width < 25) && (rect.height > 15 && rect.height < 25)) {
+			if (rect.area() > 200 && rect.area() < 600) {
+				if (((rect.y > 0 && rect.y < 60) || (rect.y > 280 && rect.y < hBoxP_StuID)) && (rect.x < 38)
+						&& (rect.width > 13 && rect.width < 25) && (rect.height > 13 && rect.height < 25)) {
 					rectTree.add(rect);
 				}
 			}
 		}
-//		System.err.println(rectTree);
 		return rectTree;
 	}
 
 	// lấy ra mã đề or MSSV
 	public String getCodeID() {
-		List<MatOfPoint> contours = MatProcess
-				.getContour(imgPlan_StuID(MatProcess.threshold(matPathImg, wBoxPlan_StuID, hBoxP_StuID)));
+		List<MatOfPoint> contours = MatProcess.getContour(imgPlan_StuID(MatProcess.threshold(matPathImg,wBoxPlan_StuID, hBoxP_StuID)));
 		Set<Rect> rectTree = new TreeSet<Rect>(SortPlantainY.SORT_PLANTAIN);
 		for (int i = 0; i < contours.size(); i++) {
 			Rect rect = Imgproc.boundingRect(contours.get(i));
-//			System.out.println(rect);
-			if (rect.area() > 230 && rect.area() < 750) {
-				if ((rect.y > 0 && rect.y < hBoxP_StuID) && (rect.x > 0 && rect.x < wPlan_StuID)
-						&& (rect.width > 13 && rect.width < 30) && (rect.height > 13 && rect.height < 30)) {
+			if (rect.area() > 100 && rect.area() < 750) {
+				if ((rect.y > 0 && rect.y < hPlan_StuID) && (rect.x > 5 && rect.x < wPlan_StuID)
+						&& (rect.width > 10&& rect.width < 28) && (rect.height > 10 && rect.height < 28)) {
 					rectTree.add(rect);
 				}
 			}
@@ -102,7 +91,6 @@ public class Plantain_StudentID {
 		for (Rect rect : rectTree) {
 			res += getnumberRow(rect, rectTree.size());
 		}
-//		System.err.println(rectTree);
 		return res;
 	}
 
@@ -110,27 +98,24 @@ public class Plantain_StudentID {
 	public String getnumberRow(Rect rect, int size) {
 		String res = "";
 		int count = 9;
-		int coordinates = 330;
+		int coordinates = 310;
 		while (count != -1) {
-//			System.err.println(coordinates);
-			if (rect.y < coordinates && rect.y > coordinates - 35) {
-
+			if (rect.y <= coordinates && rect.y >= coordinates - 31) {
 				res = String.valueOf(count);
 				break;
 			} else {
 				count--;
-				coordinates -= 35;
+				coordinates -= 31;
 				continue;
-			}
+			}		
 		}
 		return res;
 	}
-
 	// crop lấy kết quả của các ô tô đen
 	public Mat cropImgResult(int xP_S, int yP_S, int wP_S, int hP_S) {
-		Mat mat = new Mat(MatProcess.threshold(matPathImg, wBoxPlan_StuID, hBoxP_StuID),
-				new Rect(xP_S, yP_S, wP_S, hP_S));
+		Mat mat = new Mat(MatProcess.threshold(matPathImg,wBoxPlan_StuID, hBoxP_StuID), new Rect(xP_S, yP_S, wP_S, hP_S));
 		return MatProcess.reSize(mat, wPlan_StuID, hPlan_StuID);
 	}
 
+	
 }
