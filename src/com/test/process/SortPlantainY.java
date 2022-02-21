3f034d1b9425551437d79edf9ad71eec0c4bb169
Vũ Văn Minh <60693215+quanglecs@users.noreply.github.com>
2022-02-21 21:32:36 +0700

    Update SortPlantainY.java

--- src/com/test/process/SortPlantainY.java
+++ src/com/test/process/SortPlantainY.java
@@ -8,10 +8,21 @@ public class SortPlantainY {
 
 		@Override
 		public int compare(Rect o1, Rect o2) {
-			if (Math.abs(o1.x - o2.x) <= 10) // cùng 1 cột loại bỏ
+			if (Math.abs(o1.x - o2.x) <= 18) // cùng 1 cột loại bỏ
 				return 0;
 			else
 				return Integer.compare(o1.x, o2.x); // khác cột sắp xếp theo x
+		}
+
+	};
+	public static final Comparator<Rect> SORT_PLANTAIN_X = new Comparator<Rect>() {
+
+		@Override
+		public int compare(Rect o1, Rect o2) {
+			if (o1.x > o2.x)
+				return 1;
+			else
+				return -1;
 		}
 
 	};
