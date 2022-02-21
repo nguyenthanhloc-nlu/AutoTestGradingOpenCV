e420fbe313c214bec95286b1160c99838f2204fe
Thanh Van <78599547+Jonathan20082001@users.noreply.github.com>
2022-02-21 20:55:23 +0700

    Update ResltExamFrame.java

--- src/com/test/grading/ResltExamFrame.java
+++ src/com/test/grading/ResltExamFrame.java
@@ -6,17 +6,15 @@ import org.opencv.core.Rect;
 public class ResltExamFrame {
 
 	public static final double X_DAP_AN_PERCENT = 0.1;
-	public static final double Y_DAP_AN_PERCENT = 0.4755;
-	public static final double H_DAP_AN_PERCENT = 0.5245;
+	public static final double Y_DAP_AN_PERCENT = 0.452;
 
 	public Mat getAnswerFrame(Mat src) {
 		int x = (int) (X_DAP_AN_PERCENT * src.cols());
-		int y = (int) (Y_DAP_AN_PERCENT * src.rows() - 10);
-		int w = (int) ((1 - X_DAP_AN_PERCENT) * src.cols() - 10);
-		int h = (int) (H_DAP_AN_PERCENT * src.rows());
+		int y = (int) (Y_DAP_AN_PERCENT * src.rows());
+		int w = (int) ((1 - X_DAP_AN_PERCENT) * src.cols() - 20);
+		int h = (int) ((1 - Y_DAP_AN_PERCENT) * src.rows());
 
 		Mat res = new Mat(src, new Rect(x, y, w, h));
-
 		return res;
 	}
 }
