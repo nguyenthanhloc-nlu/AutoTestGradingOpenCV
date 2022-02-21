fbeb7e314a3b52643b38e2ac7fcb3d7bc738c57f
Vũ Văn Minh <60693215+quanglecs@users.noreply.github.com>
2022-02-21 21:34:03 +0700

    Update Plantain_StudentID.java

--- src/com/test/process/Plantain_StudentID.java
+++ src/com/test/process/Plantain_StudentID.java
@@ -8,6 +8,7 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class Plantain_StudentID {
@@ -41,6 +42,7 @@ public class Plantain_StudentID {
 			this.wPlan_StuID = wPlan;
 		}
 	}
+
 	// crop lấy ra ô đen mã đề or MSSV
 	public Mat imgPlan_StuID(Mat boxImgPlan_stuID) {
 		Set<Rect> listRect = getTwoNumber(boxImgPlan_stuID);
@@ -53,40 +55,50 @@ public class Plantain_StudentID {
 		int yP_S = rectOn.y + rectOn.height;
 		int wP_S = Math.abs(wBoxPlan_StuID - xP_S);
 		int hP_S = Math.abs(rectBottom.y - yP_S);
+
 		return cropImgResult(xP_S, yP_S, wP_S, hP_S);
 
 	}
 
 	// lấy vị trí 2 ô vuông định vị mã đề or MSSV
 	public Set<Rect> getTwoNumber(Mat src) {
+//		Imgcodecs.imwrite("src/img/box.jpg", src);
 		List<MatOfPoint> contours = MatProcess.getContour(src);
 		// lọc phần tử gây nhiễu
 		Set<Rect> rectTree = new TreeSet<Rect>(RectCompareNoise.RECT_COMPARE);
 		for (int i = 0; i < contours.size(); i++) {
 			Rect rect = Imgproc.boundingRect(contours.get(i));
-			if (rect.area() > 200 && rect.area() < 600) {
-				if (((rect.y > 0 && rect.y < 60) || (rect.y > 280 && rect.y < hBoxP_StuID)) && (rect.x < 38)
-						&& (rect.width > 13 && rect.width < 25) && (rect.height > 13 && rect.height < 25)) {
+//			System.err.println(rect);
+			if (rect.area() > 140 && rect.area() < 600) {
+				if (((rect.y > -1 && rect.y < 60) || (rect.y > 270 && rect.y < hBoxP_StuID))
+						&& (rect.x > -1 && rect.x <= 48) && (rect.width > 11 && rect.width < 26)
+						&& (rect.height > 10 && rect.height < 26)) {
 					rectTree.add(rect);
+
 				}
 			}
 		}
-		return rectTree;
+//		System.err.println(getSquareXMin(rectTree));
+		return getSquareXMin(rectTree);
 	}
 
 	// lấy ra mã đề or MSSV
 	public String getCodeID() {
-		List<MatOfPoint> contours = MatProcess.getContour(imgPlan_StuID(MatProcess.threshold(matPathImg,wBoxPlan_StuID, hBoxP_StuID)));
+		List<MatOfPoint> contours = MatProcess
+				.getContour(imgPlan_StuID(MatProcess.threshold(matPathImg, wBoxPlan_StuID, hBoxP_StuID)));
+
 		Set<Rect> rectTree = new TreeSet<Rect>(SortPlantainY.SORT_PLANTAIN);
 		for (int i = 0; i < contours.size(); i++) {
 			Rect rect = Imgproc.boundingRect(contours.get(i));
-			if (rect.area() > 100 && rect.area() < 750) {
-				if ((rect.y > 0 && rect.y < hPlan_StuID) && (rect.x > 5 && rect.x < wPlan_StuID)
-						&& (rect.width > 10&& rect.width < 28) && (rect.height > 10 && rect.height < 28)) {
+//			System.out.println(rect);
+			if (rect.area() > 85 && rect.area() < 750) {
+				if ((rect.y > -1 && rect.y < hPlan_StuID) && (rect.x > 0 && rect.x < wPlan_StuID)
+						&& (rect.width >= 8 && rect.width < 28) && (rect.height > 7 && rect.height < 28)) {
 					rectTree.add(rect);
 				}
 			}
 		}
+//		System.err.println(rectTree);
 		String res = "";
 		for (Rect rect : rectTree) {
 			res += getnumberRow(rect, rectTree.size());
@@ -107,15 +119,34 @@ public class Plantain_StudentID {
 				count--;
 				coordinates -= 31;
 				continue;
-			}		
+			}
 		}
 		return res;
 	}
+
 	// crop lấy kết quả của các ô tô đen
 	public Mat cropImgResult(int xP_S, int yP_S, int wP_S, int hP_S) {
-		Mat mat = new Mat(MatProcess.threshold(matPathImg,wBoxPlan_StuID, hBoxP_StuID), new Rect(xP_S, yP_S, wP_S, hP_S));
+		Mat mat = new Mat(MatProcess.threshold(matPathImg, wBoxPlan_StuID, hBoxP_StuID),
+				new Rect(xP_S, yP_S, wP_S, hP_S));
 		return MatProcess.reSize(mat, wPlan_StuID, hPlan_StuID);
 	}
 
-	
+	public Set<Rect> getSquareXMin(Set<Rect> rectTree) {
+		Set<Rect> tree = new TreeSet<Rect>(SortPlantainY.SORT_PLANTAIN_X);
+		int count = 2;
+		for (Rect rect : rectTree) {
+			if (count == 0) {
+				break;
+			} else
+				tree.add(rect);
+			count--;
+
+		}
+		Set<Rect> res = new TreeSet<Rect>(RectCompareNoise.RECT_COMPARE);
+		for (Rect rect2 : tree) {
+			res.add(rect2);
+
+		}
+		return res;
+	}
 }
