22b8b81e381836d5c6be5ebfcc6cef4ba8164b96
Thanh Van <78599547+Jonathan20082001@users.noreply.github.com>
2022-02-22 14:04:51 +0700

    Update IdStudentFrame.java

--- src/com/test/grading/IdStudentFrame.java
+++ src/com/test/grading/IdStudentFrame.java
@@ -2,7 +2,6 @@ package com.test.grading;
 
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
-import org.opencv.imgcodecs.Imgcodecs;
 
 public class IdStudentFrame {
 
@@ -17,9 +16,7 @@ public class IdStudentFrame {
 		int w = (int) (W_ID_STUDENT_PERCENT * start.cols());
 		int h = (int) (H_ID_STUDENT_PERCENT * start.rows());
 		Mat getStubAnswerFrame = new Mat(start, new Rect(x, y, w, h));
-		Imgcodecs.imwrite("src/img/mssv-b1-res.jpg", getStubAnswerFrame);
 		return getStubAnswerFrame;
 	}
-	
 
 }
